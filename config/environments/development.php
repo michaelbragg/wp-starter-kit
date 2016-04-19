f90605befc9f067b992dc2383dcef14ae1ac7d2a
Michael Bragg <michael@michaelbragg.net>
2016-04-19 08:41:49 +0100

    QA: Update development settings to meet WP Coding Standards

--- config/environments/development.php
+++ config/environments/development.php
@@ -3,10 +3,10 @@
 
 define( 'WP_LOCAL_DEV', true );
 
-define('SAVEQUERIES', true);
-define('WP_DEBUG', true);
+define( 'SAVEQUERIES', true );
+define( 'WP_DEBUG', true );
 define( 'WP_DEBUG_LOG', true );
-define('SCRIPT_DEBUG', true);
+define( 'SCRIPT_DEBUG', true );
 
 #define('WP_PROXY_HOST', 'proxy');
 #define('WP_PROXY_PORT', '3128');
