e4b8687aa8f8e689003ba44707ca5abb2badc71a
Michael Bragg <michael@michaelbragg.net>
2016-04-19 08:40:58 +0100

    QA: Add debug log for local dev

--- config/environments/development.php
+++ config/environments/development.php
@@ -1,7 +1,11 @@
 <?php
 /* Development */
+
+define( 'WP_LOCAL_DEV', true );
+
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
+define( 'WP_DEBUG_LOG', true );
 define('SCRIPT_DEBUG', true);
 
 #define('WP_PROXY_HOST', 'proxy');
