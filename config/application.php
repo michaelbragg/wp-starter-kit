d85da7c1ffd32e452dbccb23484b2325e0670273
Michael Bragg <michael@michaelbragg.net>
2016-06-15 14:14:11 +0100

    Fix typo

--- config/application.php
+++ config/application.php
@@ -72,7 +72,7 @@ define( 'WP_POST_REVISIONS', 3 );
 define( 'AUTOSAVE_INTERVAL', 120 );
 define( 'WP_MEMORY_LIMIT', '96M' );
 define( 'WP_DEFAULT_THEME', getenv( 'DEFAULT_THEME' ) ?: 'twentysixteen' );
-define( 'WPLANG', getenv( 'WPENV' ) ?: 'en_GB' );
+define( 'WPLANG', getenv( 'WPLANG' ) ?: 'en_GB' );
 
 
 /**
